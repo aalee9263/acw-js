e7ccfa26aa4e5abbb39d0dc6f227010150c5a310
Arshad Ali <aalee9263@gmail.com>
2024-08-14 23:00:23 +0500

    video time is 4:20:00

---
+++ array.js
@@ -0,0 +1,45 @@
+// Variables
+
+
+var stuNames = ["Arshad" ,"Ali", "Attari"] ;
+var stuMarks = ["100" ,"90", "80"] ;
+// console.log(stuNames[0]);
+// document.write(stuNames[0] + "<br>" + stuMarks[0]);
+// document.write(stuMarks[0]);
+
+var studentNames = [];
+studentNames[0] = "Arshad";
+studentNames[1] = "Ali";
+studentNames[2] = "Attari";
+
+
+// console.log(studentNames);
+// document.write(studentNames);
+// document.write(stuNames)
+//last erray to delete
+//last erray to delete
+// //last erray to delete
+// console.log(stuNames);
+// stuNames.pop()
+// // document.write(stuNames);
+// console.log(stuNames)
+
+
+//........splice function.........////
+// console.log(stuNames);
+// stuNames.splice(0,2);
+// console.log(stuNames);
+
+//........push function.........////
+// console.log(stuNames);
+stuNames.push("Jutt");
+// console.log(stuNames);
+
+
+//........sort & reverse .......////
+// miscNames = ["apple","mango","banana","cat", "dog"];
+// console.log(miscNames);
+// miscNames.sort()
+// console.log(miscNames);
+
+
---
+++ object.js
@@ -0,0 +1,33 @@
+//............object ................/
+
+/*
+//....1
+
+var car = {
+    name: "BMW",
+    model: "X5",
+    year: 2020
+};
+
+//....2
+
+console.log(car);
+car.year = "2024";
+console.log(car.year);
+console.log(car.name);
+console.log(car.model);
+
+
+//JSON JavaScript Objec Notation
+//....3
+
+var convertedStringFromObj = JSON.stringify(car);
+console.log(car);
+console.log(convertedStringFromObj);
+
+//....4
+var convertedObjFromString = JSON.parse(convertedStringFromObj);
+console.log(convertedObjFromString);
+
+*/
+
